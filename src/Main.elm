8fc1e185524ca5ed3bb35b9f76c08c920a5a2553
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-09-25 15:46:35 -0400

    header and input filter work

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, h1, text)
-import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (class, placeholder, type_)
+import Html.Events exposing (onClick, onInput)
 
 
 endpoint : String
@@ -50,14 +50,49 @@ update _ model =
 -- VIEW
 
 
+viewFilterInput : Html Msg
+viewFilterInput =
+    form [ class "tw-relative" ]
+        [ input
+            [ class "tw-w-full"
+            , class "tw-text-sm tw-text-black"
+            , class "tw-placeholder-gray-500"
+            , class "tw-border tw-border-gray-200"
+            , class "tw-rounded-md"
+            , class "tw-py-2"
+            , class "tw-pl-10"
+            , onInput (\_ -> NoOp)
+            , placeholder "Filter Links"
+            , type_ "text"
+            ]
+            []
+        ]
+
+
+viewLinkCard : Html msg
+viewLinkCard =
+    li [] []
+
+
 view : Model -> Browser.Document Msg
 view _ =
     { title = "Baggylinks"
     , body =
-        [ div []
-            [ div [ class "tw-flex tw-justify-end" ]
-                [ h1 [ class "tw-prose-2xl" ] [ text "Hello World" ]
+        [ section [ class "tw-w-1/2", class "tw-mx-auto" ]
+            [ header [ class "tw-flex", class "tw-justify-between", class "tw-align-center" ]
+                [ h2 [ class "tw-prose-xl" ] [ text "Links" ]
+                , button
+                    [ class "tw-bg-blue-300 hover:tw-bg-blue-400"
+                    , class "tw-rounded-md"
+                    , class "tw-text-blue-600 hover:tw-text-blue-800"
+                    , class "tw-text-sm tw-font-medium"
+                    , class "tw-px-4 tw-py-2"
+                    , onClick NoOp
+                    ]
+                    [ text "+ New" ]
                 ]
+            , viewFilterInput
+            , ul [] []
             ]
         ]
     }
